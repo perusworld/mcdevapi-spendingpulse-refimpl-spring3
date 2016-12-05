ba93139553fa7a4e9ac9f5ac38211dd2795880cd
Saravana Perumal Shanmugam <saravanaperumal@msn.com>
2016-12-05 14:34:17 -0800

    link client app

--- src/main/java/com/yosanai/web/model/InsightsRequest.java
+++ src/main/java/com/yosanai/web/model/InsightsRequest.java
@@ -1,5 +1,8 @@
 package com.yosanai.web.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class InsightsRequest {
 	protected String country;
 	protected String sector;
